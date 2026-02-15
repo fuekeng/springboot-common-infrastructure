a97eae4c9aef57deba4df24f41d9c53cbfeb3606
fuekeng <fuekengbilly@gmail.com>
2026-02-15 15:48:50 +0100

    remove acm

--- monitoring/data.tf
+++ monitoring/data.tf
@@ -8,11 +8,11 @@ data "aws_eks_cluster_auth" "cluster" {
   name = data.aws_eks_cluster.cluster.name
 }
 
-data "aws_acm_certificate" "acm" {
-  domain = var.domain_name   # Use wildcard or specific domain
-  types  = ["AMAZON_ISSUED"]    # Filter for Amazon-issued certificates
-  most_recent = true            # Use the most recently issued certificate
-}
+# data "aws_acm_certificate" "acm" {
+#   domain = var.domain_name   # Use wildcard or specific domain
+#   types  = ["AMAZON_ISSUED"]    # Filter for Amazon-issued certificates
+#   most_recent = true            # Use the most recently issued certificate
+# }
 
 data "aws_vpc" "selected" {
   filter {
