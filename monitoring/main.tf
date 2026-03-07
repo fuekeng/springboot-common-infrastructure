07915584102b07368de9cc85cd17dbcd4a8c7cb1
fuekeng <fuekengbilly@gmail.com>
2026-03-07 09:11:56 +0100

    update

--- monitoring/main.tf
+++ monitoring/main.tf
@@ -1,10 +1,10 @@
 resource "helm_release" "prometheus" {
   create_namespace = true
-  name             = "prometheus"
-  namespace        = "monitoring"
-  repository       = "https://prometheus-community.github.io/helm-charts"
-  chart            = "prometheus"
-  version          = "25.27.0" # Version stable actuelle de la stack Prometheus (v25.x)
+  name       = "prometheus"
+  namespace  = "monitoring"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "prometheus"
+  version    = "25.27.0"
 
   values = [templatefile("${var.environment}/values_prom.yaml", {
     DESTINATION_GMAIL_ID = var.DESTINATION_GMAIL_ID
@@ -16,31 +16,37 @@ resource "helm_release" "prometheus" {
     name  = "server.prefixURL"
     value = "/prometheus"
   }
+
+  set {
+    name  = "server.baseURL"
+    value = "http://localhost/prometheus"
+  }
 }
 
 resource "helm_release" "grafana" {
   create_namespace = true
-  name             = "grafana"
-  namespace        = "monitoring"
-  repository       = "https://grafana.github.io/helm-charts"
-  chart            = "grafana"
-  version          = "8.5.0"
+  name       = "grafana"
+  namespace  = "monitoring"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "grafana"
+  version    = "8.5.0"
 
   values = [
-    file("${var.environment}/values_grafana.yaml"),
-
-    yamlencode({
-      grafana.ini = {
-        server = {
-          root_url           = "%(protocol)s://%(domain)s/grafana"
-          serve_from_sub_path = true
-        }
-      }
-    })
+    file("${var.environment}/values_grafana.yaml")
   ]
 
   set {
     name  = "adminPassword"
     value = "admin"
+  }
+
+  set {
+    name  = "grafana\\.ini.server.root_url"
+    value = "%(protocol)s://%(domain)s/grafana/"
+  }
+
+  set {
+    name  = "grafana\\.ini.server.serve_from_sub_path"
+    value = "true"
   }
 }
